0898b9c45ca0304e924337a505b5e19a83ce9c9c
Ayush Shrestha <ayushshrestha8920@protonmail.com>
2024-07-08 14:57:07 +0545

    new algorithm

--- lab_6/subset_sum.cpp
+++ lab_6/subset_sum.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 #include <vector>
+#include "gettime.h"
 using namespace std;
+
+class SubsetSumSolver
+{
+private:
+    vector<int> set;
+    int targetSum;
+    vector<vector<int>> solutions;
+
+    void backtrack(vector<int> &currentSubset, int currentSum, int index)
+    {
+        if (currentSum == targetSum)
+        {
+            solutions.push_back(currentSubset);
+            return;
+        }
+        if (currentSum > targetSum || index >= set.size())
+        {
+            return;
+        }
+        currentSubset.push_back(set[index]);
+        backtrack(currentSubset, currentSum + set[index], index + 1);
+        currentSubset.pop_back();
+        backtrack(currentSubset, currentSum, index + 1);
+    }
+
+public:
+    SubsetSumSolver(const vector<int> &s, int target)
+    {
+        this->set = s;
+        this->targetSum = target;
+    }
+
+    vector<vector<int>> solve()
+    {
+        vector<int> currentSubset;
+        backtrack(currentSubset, 0, 0);
+        return solutions;
+    }
+};
+
+class SubsetSumExecutor
+{
+public:
+    static void execute(int size)
+    {
+        vector<int> set;
+        int targetSum = 50;
+        for (int i = 0; i < size; i++)
+        {
+            set.push_back(rand() % 10);
+        }
+        SubsetSumSolver solver(set, targetSum);
+        vector<vector<int>> solutions;
+        long long time = getTime([&]()
+                                 { solutions = solver.solve(); });
+        cout << "Size = " << size << "Time = " << time << endl;
+    }
+};
 
 bool flag = 0;
 void PrintSubsetSum(int i, int n, int set[], int targetSum,
@@ -40,25 +99,17 @@ void PrintSubsetSum(int i, int n, int set[], int targetSum,
 
 int main()
 {
-    // Test case 1
-    int set[] = {1, 2, 1};
-    int sum = 3;
-    int n = sizeof(set) / sizeof(set[0]);
-    vector<int> subset;
-    cout << "Output 1:" << endl;
-    PrintSubsetSum(0, n, set, sum, subset);
-    cout << endl;
-    flag = 0;
-    // Test case 2
-    int set2[] = {3, 34, 4, 12, 5, 2};
-    int sum2 = 30;
-    int n2 = sizeof(set) / sizeof(set[0]);
-    vector<int> subset2;
-    cout << "Output 2:" << endl;
-    PrintSubsetSum(0, n2, set2, sum2, subset2);
-    if (!flag)
+    vector<int> sizes;
+    int start_size = 15;
+    int increment = 1;
+    sizes.push_back(start_size);
+    srand(time(NULL));
+    SubsetSumExecutor::execute(start_size);
+    for (size_t i = 0; i < 4; i++)
     {
-        cout << "There is no such subset";
+        start_size += increment;
+        sizes.push_back(start_size);
+        SubsetSumExecutor::execute(start_size);
     }
 
     return 0;
